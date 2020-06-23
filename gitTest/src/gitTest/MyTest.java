bda4fc7c6b676ad3df78f871ecf248c0e6b7d4c2
주말반 <@DESKTOP-GD76PAT>
2020-06-23 17:56:14 +0900

    자바파일만 수정

--- gitTest/src/gitTest/MyTest.java
+++ gitTest/src/gitTest/MyTest.java
@@ -3,6 +3,6 @@ package gitTest;
 public class MyTest {
 	public static void main(String[] args) {
 		System.out.println("Git 연동!");
-		System.out.println("연동 수정");
+		System.out.println("수정..수정");
 	}
 }
